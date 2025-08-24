9413ec04a97014b1d6a214d0a62eefda46eaae62
Ayush Ranjan <codingwithayush10@gmail.com>
2025-08-25 03:29:00 +0530

    Add files via upload

---
+++ components/login.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Login Screen</Text>
+      {/* You will add login form code here later */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
